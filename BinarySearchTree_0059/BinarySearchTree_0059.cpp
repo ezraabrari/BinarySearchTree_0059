05ab88710e1ab1f02921c9225e33a625f1029e43
ezraabrari <ezraabrari13@gmail.com>
2024-06-14 14:08:07 +0700

    Membuat prosedur inorder dengan variabel node ptr dimana jika root dama dengan NULL maka akan menunjukkan tampilan "tree is empty" dan ijka ptr tidak sama dengan NULL maka akan menampilkan info dari ptr

--- BinarySearchTree_0059/BinarySearchTree_0059.cpp
+++ BinarySearchTree_0059/BinarySearchTree_0059.cpp
@@ -58,4 +58,16 @@ public:
 				currentNode = currentNode->rightchild;
 		}
 	}
+
+	void inorder(Node* ptr) {
+		if (ROOT == NULL) {
+			cout << "tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL) {
+			inorder(ptr->leftchild);
+			cout << ptr->info << "  ";
+			inorder(ptr->rightchild);
+		}
+	}
 };
