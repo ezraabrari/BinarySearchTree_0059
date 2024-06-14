0bb4d6b8bcbe684d6302b4abfb4979518045f088
ezraabrari <ezraabrari13@gmail.com>
2024-06-14 13:58:22 +0700

    Membuat conditional statement dimana ketika parent null maka Root akan ditandakan sebagai node baru dan ketika nilai element lebih kecil dari paret maka akan membuat left child dari parent sebagai node baru, dan ketika nilai elemen lebih besar maka akan membuat rightchild menjadi node baru

--- BinarySearchTree_0059/BinarySearchTree_0059.cpp
+++ BinarySearchTree_0059/BinarySearchTree_0059.cpp
@@ -33,5 +33,17 @@ public:
 		Node* currentNode = NULL;
 		search(element, parent, currentNode);
 
+		if (parent == NULL) {
+			ROOT = newNode;
+			return;
+		}
+
+		if (element < parent->info) {
+			parent->leftchild = newNode;
+		}
+
+		else if(element > parent->info) {
+			parent->rightchild = newNode;
+		}
 	}
 };
