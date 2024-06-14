8dc623350d1d840688f4ea40f134477452e88e3d
ezraabrari <ezraabrari13@gmail.com>
2024-06-14 14:20:58 +0700

    Membuat case 1 untuk memilih prosedur insert dengan memasuki kata kata yang ingin dimasuki

--- BinarySearchTree_0059/BinarySearchTree_0059.cpp
+++ BinarySearchTree_0059/BinarySearchTree_0059.cpp
@@ -113,6 +113,14 @@ int main() {
 		cout << endl;
 
 		switch (ch) {
+		case '1':
+		{
+			cout << "Enter a word :";
+			string word;
+			cin >> word;
+			obj.insert(word);
+			break;
+		}
 
 		}
 	}
